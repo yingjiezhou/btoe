4565db3419680dbc890d20f77b3db8669615cf61
Yingjie Zhou <yingjie.zhou@cern.ch>
2020-09-20 20:19:37 +0800

    makePlot_def.C -> makebFPlot.C, add eg: gpt and def compa

--- pp_run12/offline/ptrack_template/script/makebFPlot.C
+++ pp_run12/offline/ptrack_template/script/makebFPlot.C
@@ -1,15 +1,17 @@
 void drawtext();
 TGraph *getFonll();
-void makePlot_def(const char* FileName="Sep16"){
-  
-    gROOT->ProcessLine(".x ~/myStyle.C");
+TGraphAsymmErrors *getSysb(const char* sys = "gpt", const char* def = "default");
+void makebFPlot(const char* FileName="Sep16"){
+  gROOT->ProcessLine(".x ~/myStyle.C");
   //  gROOT->SetBatch(kTRUE);
   //  gStyle->SetOptFit(1111);
   char name[100];
-  auto fyjd = new TFile(Form("FFOutput/sysChange_default_%s_FIT.root", FileName),"READ");
-  auto hd = (TGraphAsymmErrors *)fyjd->Get("HT0");
+  const char* outfileName = "gptVsDef";
+  TGraphAsymmErrors*  ggpt = getSysb("gpt", FileName);
+
+  auto fyjd = new TFile(Form("FFOutput/sys/sysChange_default_%s_FIT.root", FileName),"READ");
+  auto hd = (TGraphAsymmErrors *)fyjd->Get("sys");
   auto hnorm = (TGraphAsymmErrors *)fyjd->Get("normsmall");
-  
   //===========================================================================
   // preliminary, shift x axis
   float ypT[9], yRb[9], ydyU[9], ydyD[9], ydysU[9], ydysD[9];
@@ -77,7 +79,7 @@ void makePlot_def(const char* FileName="Sep16"){
   TGraphAsymmErrors* syst6 = new TGraphAsymmErrors(7,pT6,Rb6,dx2,dx2,dysD6,dysU6);
   //=======================================================================
   
-
+  
   TGraph *FONLL[3];
   FONLL[0] = new TGraph("input/FONLL_BAND_UP.dat");
   FONLL[1] = new TGraph("input/FONLL_BAND_DW.dat");
@@ -100,11 +102,11 @@ void makePlot_def(const char* FileName="Sep16"){
   stat->SetMarkerSize(1.5);
   stat->SetMarkerColor(kRed);
   stat->SetLineColor(kRed);
-//  stat->GetYaxis()->SetRangeUser(-0.2,1.2);
-//  stat->GetXaxis()->SetRangeUser(0,12);
+  //  stat->GetYaxis()->SetRangeUser(-0.2,1.2);
+  //  stat->GetXaxis()->SetRangeUser(0,12);
   hdf->GetXaxis()->SetTitle("NPE p_{T} (GeV/c)");
   hdf->GetYaxis()->SetTitle("B#rightarrowNPE / Inclusive#rightarrowNPE");
-
+  
   syst->GetYaxis()->SetRangeUser(-0.1,1.2);
   syst->GetXaxis()->SetRangeUser(0,12);
   stat->GetXaxis()->SetLimits(0, 12);
@@ -112,11 +114,11 @@ void makePlot_def(const char* FileName="Sep16"){
   
   syst6->GetXaxis()->SetLimits(0, 12);
   stat6->GetXaxis()->SetLimits(0, 12);
-
+  
   hdf->GetXaxis()->SetLimits(0,12);
   hdf->GetYaxis()->SetLimits(-0.1,1.2);
   hdf->GetYaxis()->SetRangeUser(-0.1,1.2);
-
+  
   hdf->SetMarkerSize(1.5);
   hdf->SetMarkerStyle(21);
   hdf->SetMarkerColor(kBlue);
@@ -131,6 +133,8 @@ void makePlot_def(const char* FileName="Sep16"){
   FONLL[1]->Draw("same");
   FONLL[2]->Draw("same");
   
+  ggpt->Draw("same pe"); // sys
+  
   syst6->SetLineColor(kBlack);
   syst6->SetMarkerColor(kBlack);
   syst6->SetFillStyle(0);
@@ -140,16 +144,17 @@ void makePlot_def(const char* FileName="Sep16"){
   stat6->SetMarkerSize(1.8);
   stat6->SetMarkerColor(kBlack);
   stat6->SetLineColor(kBlack);
-//  syst6->Draw("same pe2");
-//  stat6->Draw("same pe");
+  //  syst6->Draw("same pe2");
+  //  stat6->Draw("same pe");
   
   TLegend* leg = new TLegend(0.18,0.72,0.6,0.91);
   leg->SetBorderSize(0);
   leg->SetFillStyle (0);
   leg->SetFillColor (0);
-//  leg->AddEntry(stat6,"Run 5/6 p+p 200 GeV","pe");
+  //  leg->AddEntry(stat6,"Run 5/6 p+p 200 GeV","pe");
   leg->AddEntry(stat,"STAR Run 12(Prelimary)","pe");
-  leg->AddEntry(hdf,"STAR Run 12(Yingjie)","pe");
+  leg->AddEntry(hdf,"STAR Run 12(Yingjie), gp_{T}>0.3","pe");
+  leg->AddEntry(ggpt,"STAR Run 12(Yingjie), gp_{T}>0.2","pe");
   leg->AddEntry(FONLL[2],"FONLL","l");
   leg->Draw("same");
   ll->Draw("same");
@@ -166,15 +171,15 @@ void makePlot_def(const char* FileName="Sep16"){
   
   // Make PDF with output canvases
   TCanvas* temp = new TCanvas("temp","");
-
-//  TCanvas* temp = new TCanvas("temp","",50,50,1200,1200);
-  sprintf(name, "bFraction.pdf[", FileName);
+  
+  //  TCanvas* temp = new TCanvas("temp","",50,50,1200,1200);
+  sprintf(name, "pic/%s_bFraction_%s.pdf[", FileName, outfileName);
   temp->Print(name);
-//  temp = cnorm;
-//  temp->Print(name);
+  //  temp = cnorm;
+  //  temp->Print(name);
   temp = c1;
   temp->Print(name);
-  sprintf(name, "bFraction.pdf]", FileName);
+  sprintf(name, "pic/%s_bFraction_%s.pdf]", FileName, outfileName);
   temp->Print(name);
   //============================================================================================
   
@@ -216,208 +221,236 @@ void drawtext()
   }
 }
 TGraph *getFonll(){
-   Double_t Graph0_fx1201[99] = {
-//     0.121,
-//     0.202,
-//     0.303,
-//     0.404,
-//     0.5051,
-//     0.6061,
-//     0.7071,
-//     0.8081,
-//     0.9091,
-//     1.0101,
-//     1.1111,
-//     1.2121,
-//     1.3131,
-//     1.4141,
-//     1.5151,
-//     1.6162,
-//     1.7172,
-//     1.8182,
-//     1.9192,
-     2.0202,
-     2.1212,
-     2.2222,
-     2.3232,
-     2.4242,
-     2.5252,
-     2.6263,
-     2.7273,
-     2.8283,
-     2.9293,
-     3.0303,
-     3.1313,
-     3.2323,
-     3.3333,
-     3.4343,
-     3.5354,
-     3.6364,
-     3.7374,
-     3.8384,
-     3.928278,
-     4.0404,
-     4.1414,
-     4.2424,
-     4.3434,
-     4.4444,
-     4.5454,
-     4.6465,
-     4.7475,
-     4.8485,
-     4.9495,
-     5.0505,
-     5.1515,
-     5.2525,
-     5.3535,
-     5.4546,
-     5.5556,
-     5.6566,
-     5.7576,
-     5.8586,
-     5.9596,
-     6.0606,
-     6.1616,
-     6.2626,
-     6.3636,
-     6.4646,
-     6.5657,
-     6.6667,
-     6.7677,
-     6.8687,
-     6.9697,
-     7.0707,
-     7.1717,
-     7.2727,
-     7.3737,
-     7.4748,
-     7.5758,
-     7.6768,
-     7.7778,
-     7.8788,
-     7.9798,
-     8.0808,
-     8.1818,
-     8.2828,
-     8.3838,
-     8.4848,
-     8.5859,
-     8.6869,
-     8.7879,
-     8.8889,
-     8.9899,
-     9.0909,
-     9.1919,
-     9.2929,
-     9.3939,
-     9.4949,
-     9.596,
-     9.697,
-     9.798,
-     9.899,
-     10};
-   Double_t Graph0_fy1201[99] = {
-//     0.0002157487,
-//     0.0006701205,
-//     0.001142921,
-//     0.001860684,
-//     0.002967843,
-//     0.00464543,
-//     0.007151315,
-//     0.01081023,
-//     0.01583555,
-//     0.02250377,
-//     0.03105067,
-//     0.04170598,
-//     0.05436157,
-//     0.06906901,
-//     0.0856345,
-//     0.103641,
-//     0.123014,
-//     0.143373,
-//     0.1643714,
-     0.1855542,
-     0.2066991,
-     0.227445,
-     0.2478147,
-     0.2675238,
-     0.2863677,
-     0.3044559,
-     0.3217666,
-     0.3380942,
-     0.3533731,
-     0.3678674,
-     0.3816963,
-     0.3944309,
-     0.4066002,
-     0.417999,
-     0.4287498,
-     0.4389604,
-     0.4486786,
-     0.4576232,
-     0.468439,
-     0.47433,
-     0.4822119,
-     0.489415,
-     0.4966418,
-     0.5032611,
-     0.5096177,
-     0.5157564,
-     0.5215858,
-     0.5272161,
-     0.5327155,
-     0.5378175,
-     0.5429145,
-     0.5477484,
-     0.5524398,
-     0.5569227,
-     0.5613725,
-     0.5655629,
-     0.5697577,
-     0.5736638,
-     0.5775213,
-     0.5814537,
-     0.5850663,
-     0.5887076,
-     0.592345,
-     0.5956383,
-     0.5990272,
-     0.6023273,
-     0.6055186,
-     0.6086593,
-     0.6116868,
-     0.614716,
-     0.6176296,
-     0.6204948,
-     0.6234068,
-     0.6261045,
-     0.6288355,
-     0.6315031,
-     0.6340331,
-     0.6365819,
-     0.6390586,
-     0.6415495,
-     0.6440217,
-     0.6462751,
-     0.6487198,
-     0.6508495,
-     0.6532281,
-     0.655332,
-     0.657531,
-     0.6596438,
-     0.6617635,
-     0.6637141,
-     0.6657618,
-     0.6677515,
-     0.6696993,
-     0.6716243,
-     0.6735384,
-     0.6753587,
-     0.6771645,
-     0.6790332,
-     0.680747};
+  Double_t Graph0_fx1201[99] = {
+    //     0.121,
+    //     0.202,
+    //     0.303,
+    //     0.404,
+    //     0.5051,
+    //     0.6061,
+    //     0.7071,
+    //     0.8081,
+    //     0.9091,
+    //     1.0101,
+    //     1.1111,
+    //     1.2121,
+    //     1.3131,
+    //     1.4141,
+    //     1.5151,
+    //     1.6162,
+    //     1.7172,
+    //     1.8182,
+    //     1.9192,
+    2.0202,
+    2.1212,
+    2.2222,
+    2.3232,
+    2.4242,
+    2.5252,
+    2.6263,
+    2.7273,
+    2.8283,
+    2.9293,
+    3.0303,
+    3.1313,
+    3.2323,
+    3.3333,
+    3.4343,
+    3.5354,
+    3.6364,
+    3.7374,
+    3.8384,
+    3.928278,
+    4.0404,
+    4.1414,
+    4.2424,
+    4.3434,
+    4.4444,
+    4.5454,
+    4.6465,
+    4.7475,
+    4.8485,
+    4.9495,
+    5.0505,
+    5.1515,
+    5.2525,
+    5.3535,
+    5.4546,
+    5.5556,
+    5.6566,
+    5.7576,
+    5.8586,
+    5.9596,
+    6.0606,
+    6.1616,
+    6.2626,
+    6.3636,
+    6.4646,
+    6.5657,
+    6.6667,
+    6.7677,
+    6.8687,
+    6.9697,
+    7.0707,
+    7.1717,
+    7.2727,
+    7.3737,
+    7.4748,
+    7.5758,
+    7.6768,
+    7.7778,
+    7.8788,
+    7.9798,
+    8.0808,
+    8.1818,
+    8.2828,
+    8.3838,
+    8.4848,
+    8.5859,
+    8.6869,
+    8.7879,
+    8.8889,
+    8.9899,
+    9.0909,
+    9.1919,
+    9.2929,
+    9.3939,
+    9.4949,
+    9.596,
+    9.697,
+    9.798,
+    9.899,
+    10};
+  Double_t Graph0_fy1201[99] = {
+    //     0.0002157487,
+    //     0.0006701205,
+    //     0.001142921,
+    //     0.001860684,
+    //     0.002967843,
+    //     0.00464543,
+    //     0.007151315,
+    //     0.01081023,
+    //     0.01583555,
+    //     0.02250377,
+    //     0.03105067,
+    //     0.04170598,
+    //     0.05436157,
+    //     0.06906901,
+    //     0.0856345,
+    //     0.103641,
+    //     0.123014,
+    //     0.143373,
+    //     0.1643714,
+    0.1855542,
+    0.2066991,
+    0.227445,
+    0.2478147,
+    0.2675238,
+    0.2863677,
+    0.3044559,
+    0.3217666,
+    0.3380942,
+    0.3533731,
+    0.3678674,
+    0.3816963,
+    0.3944309,
+    0.4066002,
+    0.417999,
+    0.4287498,
+    0.4389604,
+    0.4486786,
+    0.4576232,
+    0.468439,
+    0.47433,
+    0.4822119,
+    0.489415,
+    0.4966418,
+    0.5032611,
+    0.5096177,
+    0.5157564,
+    0.5215858,
+    0.5272161,
+    0.5327155,
+    0.5378175,
+    0.5429145,
+    0.5477484,
+    0.5524398,
+    0.5569227,
+    0.5613725,
+    0.5655629,
+    0.5697577,
+    0.5736638,
+    0.5775213,
+    0.5814537,
+    0.5850663,
+    0.5887076,
+    0.592345,
+    0.5956383,
+    0.5990272,
+    0.6023273,
+    0.6055186,
+    0.6086593,
+    0.6116868,
+    0.614716,
+    0.6176296,
+    0.6204948,
+    0.6234068,
+    0.6261045,
+    0.6288355,
+    0.6315031,
+    0.6340331,
+    0.6365819,
+    0.6390586,
+    0.6415495,
+    0.6440217,
+    0.6462751,
+    0.6487198,
+    0.6508495,
+    0.6532281,
+    0.655332,
+    0.657531,
+    0.6596438,
+    0.6617635,
+    0.6637141,
+    0.6657618,
+    0.6677515,
+    0.6696993,
+    0.6716243,
+    0.6735384,
+    0.6753587,
+    0.6771645,
+    0.6790332,
+    0.680747};
   
   auto gFonll = new TGraph(80, Graph0_fx1201, Graph0_fy1201);
- 
+  
   return gFonll;
+}
+
+TGraphAsymmErrors *getSysb(const char* sys = "gpt", const char* def = "default"){
+  
+  auto fyjd = new TFile(Form("FFOutput/sys/sysChange_%s_%s_FIT.root", sys, def),"READ");
+  auto hd = (TGraphAsymmErrors *)fyjd->Get("sys");
+  auto hnorm = (TGraphAsymmErrors *)fyjd->Get("normsmall");
+  
+  float dx[9],ypT[9], yRb[9], ydyU[9], ydyD[9];
+  
+  for(auto i = 0; i<7; i++){
+    double ty, tx;
+    hd->GetPoint(i, tx, ty);
+    cout<<tx<<endl;
+    ypT[i] = tx+0.3;  // for shift
+    yRb[i] = ty;
+    dx[i] = 0.15;
+    ydyD[i] = hd->GetErrorYlow(i);
+    ydyU[i] = hd->GetErrorYhigh(i);
+  }
+
+  TGraphAsymmErrors* hdf = new TGraphAsymmErrors(7,ypT,yRb,dx,dx,ydyD,ydyU); // zyj
+  hdf->SetMarkerSize(1.5);
+  hdf->SetMarkerStyle(21+4);
+  hdf->SetMarkerColor(kBlue);
+  hdf->SetLineColor(kBlue);
+  
+  return hdf;
 }
